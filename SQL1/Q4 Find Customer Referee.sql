2a87ccc6816de24afbb079ebfb28d627b5680c3d
neehanayak <wizardingbyte@outlook.com>
2026-03-26 18:32:41 -0400

    Create Q4 Find Customer Referee.sql

---
+++ SQL1/Q4 Find Customer Referee.sql
@@ -0,0 +1,8 @@
+-- Find the names of the customer that are either:
+-- 1. referred by any customer with id != 2.
+-- 2. not referred by any customer.
+-- Return the result table in any order.
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2 or referee_id is NULL;
