078df849f84a8a1df41ee6b5333d9c72c7605bf0
neehanayak <wizardingbyte@outlook.com>
2026-03-26 18:04:08 -0400

    Create Q1 Combine Two Tables.py

---
+++ SQL1/Q1 Combine Two Tables.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    table = pd.merge(person, address, on = 'personId', how = 'left')
+    return table[['firstName', 'lastName', 'city', 'state']]
