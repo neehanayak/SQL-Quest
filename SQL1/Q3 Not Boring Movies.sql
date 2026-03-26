2ae5e2b7120e8e96663c3526de168d296c2e85ad
neehanayak <wizardingbyte@outlook.com>
2026-03-26 18:13:06 -0400

    Create Q3 Not Boring Movies.sql

---
+++ SQL1/Q3 Not Boring Movies.sql
@@ -0,0 +1,7 @@
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+-- Return the result table ordered by rating in descending order.
+
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1 and description <> 'boring'
+ORDER BY rating DESC;
